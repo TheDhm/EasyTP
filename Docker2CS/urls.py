eea5dcce0d9ae4a699fcadd9b46c21ac829ffe93
TheDHM <dhmnopro@gmail.com>
2022-04-05 00:30:44 +0100

    rollback x)

--- Docker2CS/urls.py
+++ Docker2CS/urls.py
@@ -1,4 +1,4 @@
-"""container-manager-app URL Configuration
+"""Docker2CS URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
--- Docker2CS/wsgi.py
+++ Docker2CS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for container-manager-app project.
+WSGI config for Docker2CS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'container-manager-app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Docker2CS.settings')
 
 application = get_wsgi_application()
