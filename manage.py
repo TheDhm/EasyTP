8033eaf9c9c48195fcc5277d55fb71f1549bcc1c
TheDHM <dhmnopro@gmail.com>
2022-04-05 00:33:40 +0100

    rollback x)

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'container-manager-app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Docker2CS.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
