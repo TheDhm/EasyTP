4f497db8cbbb95c5f4a0ef9068f5bc81218f3229
TheDHM <dhmnopro@gmail.com>
2022-04-27 13:23:03 +0100

    updated form AddUsersCSV

--- main/forms.py
+++ main/forms.py
@@ -3,8 +3,8 @@ from django import forms
 from .models import DefaultUser
 
 
-class AddUsers(forms.Form):
-    csv_file = forms.FileField(label='csv file', max_length=200, allow_empty_file=False)
+class AddUsersCSV(forms.Form):
+    csv_file = forms.FileField(label='csv file', max_length=200, allow_empty_file=False, required=True)
     role = forms.ChoiceField(label='choose role for users', choices=DefaultUser.ROLES, required=True)
     year = forms.ChoiceField(label='choose year for users', choices=DefaultUser.YEARS, required=True)
 
