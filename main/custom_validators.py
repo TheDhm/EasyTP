c43b106fe01297b9c6ba93bba787e55603c14e91
TheDHM <dhmnopro@gmail.com>
2022-05-01 14:44:03 +0100

    fixed emails validator

--- main/custom_validators.py
+++ main/custom_validators.py
@@ -2,6 +2,7 @@ from django.core.validators import EmailValidator
 from django.utils.deconstruct import deconstructible
 from django.core.exceptions import ValidationError
 from pandas import read_csv, read_excel
+from django.utils.translation import gettext_lazy as _
 
 
 @deconstructible
@@ -15,17 +16,28 @@ class EsiEmailValidator(EmailValidator):
 
 
 def validate_emails_in_file(file):
-    email_validator = EsiEmailValidator()
-    print("hello ", file)
+    email_validator = EsiEmailValidator(allowlist=['esi.dz'],
+                                        message='Enter a valid "@esi.dz" email address.')
+
+    invalid_emails = []
     if str(file).endswith('.csv'):
         users = read_csv(file)
     else:
         users = read_excel(file)
 
+    file.seek(0)
+
     for email in users.iloc[:, 0]:
-        print(email)
-        email_validator(email)
+        try:
+            email_validator(email)
+        except ValidationError:
+            invalid_emails.append(email)
 
-
+    if invalid_emails:
+        raise ValidationError([
+            ValidationError(_('Invalid email: %(email)s'),
+                            params={'email': email})
+            for email in invalid_emails
+        ])
 
 
