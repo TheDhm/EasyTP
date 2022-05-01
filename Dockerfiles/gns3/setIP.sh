dda3b7cb5b573a37474977fc6c623c590e814c2b
TheDHM <dhmnopro@gmail.com>
2022-05-01 22:23:15 +0100

    updated getting ip

--- Dockerfiles/gns3/setIP.sh
+++ Dockerfiles/gns3/setIP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 1.5
-IP=$(ifconfig | awk '/inet/ {print $2}' | head -1)
+IP=$(ifconfig | grep -A1 eth0 | awk '/inet/ {print $2}')
 ACT=$(awk '/host/ {print $3}' /data/.config/GNS3/2.2/gns3_server.conf)
 awk -v ip="$IP" -v act="$ACT" '{ gsub(act, ip); print $0}' /data/.config/GNS3/2.2/gns3_server.conf > /tmp/out.conf
 cat /tmp/out.conf > /data/.config/GNS3/2.2/gns3_server.conf
