e2217790a1f44cbc1d3eb01534f190238285fc2d
TheDHM <dhmnopro@gmail.com>
2022-05-01 16:54:53 +0100

    added sending password via email

--- Docker2CS/settings.py
+++ Docker2CS/settings.py
@@ -130,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'cms.rdv.contact@gmail.com'
+EMAIL_HOST_PASSWORD = 'eqzeulcbchlhrqnq'
