f2351d4aa8362b867c1dc73c6583f039a11391cc
TheDHM <dhmnopro@gmail.com>
2022-05-11 20:24:27 +0100

    fixed stop pod url

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', views.login_request, name="login_request"),
     path('logout/', views.logout_request, name="logout_request"),
     path('start/<str:app_name>/', views.start_pod, name="start_pod"),
-    path('stop/<str:app_name>/', views.start_pod, name="stop_pod"),
+    path('stop/<str:app_name>/', views.stop_pod, name="stop_pod"),
 ]
