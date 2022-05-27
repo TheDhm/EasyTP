731a075bb663e767da00fffb0ae925237b9ce4a1
thedhm <dhmnopro@gmail.com>
2022-05-27 12:13:31 +0100

    fixed static files

--- Docker2CS/settings.py
+++ Docker2CS/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
