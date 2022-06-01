d0ac09e116386761a28b2d3ab2ee4b6f50c6b3de
thedhm <dhmnopro@gmail.com>
2022-06-01 02:23:33 +0100

    added a lot of urls

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('login/', views.login_request, name="login_request"),
     path('logout/', views.logout_request, name="logout_request"),
     path('start/<str:app_name>/', views.start_pod, name="start_pod"),
+    path('start/<str:app_name>/<slug:user_id>/', views.start_pod, name="start_pod"),
     path('stop/<str:app_name>/', views.stop_pod, name="stop_pod"),
-    path('testapps/', views.test_apps, name="test_apps")
+    path('stop/<str:app_name>/<slug:user_id>/', views.stop_pod, name="stop_pod"),
+    path('testapps/', views.test_apps, name="test_apps"),
+    path('group/', views.list_students, name="list_students"),
+    path('group/<slug:group_id>/', views.list_students, name="list_students"),
 ]
