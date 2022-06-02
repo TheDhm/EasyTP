855b35dc1edb96faa06efa82e671549df2d6e0e8
thedhm <dhmnopro@gmail.com>
2022-06-02 23:10:49 +0100

    final

--- main/urls.py
+++ main/urls.py
@@ -8,11 +8,20 @@ urlpatterns = [
     path('', views.homepage, name="homepage"),
     path('login/', views.login_request, name="login_request"),
     path('logout/', views.logout_request, name="logout_request"),
+
     path('start/<str:app_name>/', views.start_pod, name="start_pod"),
     path('start/<str:app_name>/<slug:user_id>/', views.start_pod, name="start_pod"),
+
     path('stop/<str:app_name>/', views.stop_pod, name="stop_pod"),
     path('stop/<str:app_name>/<slug:user_id>/', views.stop_pod, name="stop_pod"),
+
     path('testapps/', views.test_apps, name="test_apps"),
+
     path('group/', views.list_students, name="list_students"),
     path('group/<slug:group_id>/', views.list_students, name="list_students"),
+    path('group/<slug:group_id>/<slug:app_id>/', views.list_students, name="list_students"),
+
+    path('files/', views.file_explorer, name="file_explorer"),
+    path('files/<path>', views.file_explorer, name="file_explorer"),
+    path('download/<path>', views.download_file, name="download_file"),
 ]
